cd29ce71949b6cccb24af72a5f09adb7b754b9b0
Sigma2228 <149525498+Sigma2228@users.noreply.github.com>
2025-09-06 15:16:47 +0300

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,43 +1,43 @@
 import { BASE_URL, KEY } from "./base-api.js";
+import { COUNTRIES } from "./countries.js";
 
 const countryList = document.getElementById("countries");
-const eventsContainer = document.getElementById("events"); 
+const eventsContainer = document.getElementById("events");
 
-let events = [];
-
+let events = []; 
 
 fetch(`${BASE_URL}events.json?apikey=${KEY}`)
   .then(response => response.json())
   .then(data => {
-    events = data._embedded.events; 
-    renderCountries(events);
+    events = data._embedded.events;
+    console.log("Отримав події:", events);
   })
   .catch(err => console.error(err));
 
+Object.entries(COUNTRIES).forEach(([code, name]) => {
+  const li = document.createElement("li");
+  li.textContent = name;
+  li.addEventListener("click", () => renderEventsByCountry(code));
+  countryList.appendChild(li);
+});
 
-function renderCountries(events) {
-  const countries = [...new Set(events.map(e => e._embedded.venues[0].country.name))];
+function renderEventsByCountry(code) {
+  eventsContainer.innerHTML = ""; 
+  const filtered = events.filter(e => e._embedded.venues[0].country.countryCode === code);
 
-  countries.forEach(country => {
-    const li = document.createElement("li");
-    li.textContent = country;
-    li.addEventListener("click", () => renderEventsByCountry(country));
-    countryList.appendChild(li);
-  });
-}
-
-
-function renderEventsByCountry(country) {
-  eventsContainer.innerHTML = "";
-  const filtered = events.filter(e => e._embedded.venues[0].country.name === country);
+  if (filtered.length === 0) {
+    eventsContainer.innerHTML = `<p>Немає подій у цій країні</p>`;
+    return;
+  }
 
   filtered.forEach(ev => {
+    const venue = ev._embedded.venues[0];
     const card = document.createElement("div");
     card.classList.add("card");
     card.innerHTML = `
       <h3>${ev.name}</h3>
-      <p>${ev.dates.start.localDate}</p>
-      <p>${ev._embedded.venues[0].name} – ${ev._embedded.venues[0].city.name}</p>
+      <p>📅 ${ev.dates.start.localDate}</p>
+      <p>📍 ${venue.name}, ${venue.city.name}</p>
     `;
     eventsContainer.appendChild(card);
   });
